c63001032d5313ed383f909ab9c9910230aeba16
bssajjana <bssajjana18@gmail.com>
2024-11-19 11:38:52 +0530

    exam

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 
 
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
 
 
 
@@ -57,7 +57,7 @@ function App(){
 export default App;
 */
 
-/*
+
 import React, { Component } from 'react';
 class Counter extends Component{
   constructor(){
@@ -84,7 +84,7 @@ class Counter extends Component{
   }
 }
 export default Counter;
-*/
+
 
 // import ReactDOM from "react-dom/client";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
